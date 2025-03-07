cfa1bc1a66d3d41aa523487050aa49f610a2113e
shannytiznado <shannytiznado@gmail.com>
2025-03-07 02:51:32 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int n = 123;
+    double d = 3.14;
+    string s = "hello";
+    bool b = true;
+
+    cout << n << endl;
+    cout << d << endl;
+    cout << s << endl;
+    cout << b << endl;
+
+    return 0;
+}
