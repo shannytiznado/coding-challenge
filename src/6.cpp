c028722feefe01dfc56fd19f86df51ae37becd52
shannytiznado <shannytiznado@gmail.com>
2025-03-11 04:26:30 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,10 @@
+
+int main() {
+    int x = 5;
+    if(x < 10) {
+        cout << "x is less than 10" << endl;
+    } else {
+        cout << "x is greater than or equal to 10" << endl;
+    }
+    return 0;
+}
