43ec4921d8e9091687d1e4cbef0389aa5119d361
shannytiznado <shannytiznado@gmail.com>
2025-04-24 15:29:39 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    while(i < 5) {
+        cout << "Hello, World!" << endl;
+        ++i; // Increment 'i' by 1
+    }
+    return 0;
+}
