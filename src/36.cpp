b08f215f26eaa2a5cba7dadfdb0d99e0c1434e36
shannytiznado <shannytiznado@gmail.com>
2025-05-19 03:38:48 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Number is positive." << endl;
+    } else {
+        cout << "Number is not positive." << endl;
+    }
+    return 0;
+}
